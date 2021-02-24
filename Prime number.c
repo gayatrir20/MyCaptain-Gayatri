2aa6bf66bc141c38b6a21c088ff3b6a3c455e510
gayatrir20 <79561309+gayatrir20@users.noreply.github.com>
2021-02-24 17:58:41 +0530

    Create Prime number.c

---
+++ Prime number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main()
+{
+    int num;  
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    if (Prime(num, num/2) == 1)
+    {
+        printf("%d is a prime number",num);
+    }
+    else
+    {
+      printf("%d is  not a prime number",num);  
+    }
+    return 0;
+}
+int Prime(int n, int i)
+{
+    if(i == 1)
+    return 1;
+    else
+    {
+        if (n%i == 0)
+        return 0;
+        else
+        Prime(n,i+1);
+    }
+}
